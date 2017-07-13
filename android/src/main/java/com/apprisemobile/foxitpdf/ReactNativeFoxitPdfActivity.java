450a19cf67a6f8481397f43c98d59ea5f1f3e542
davearata <dave.arata@dacatech.com>
2017-07-13 09:44:20 -0400

    latest code from foxit team to fix crashes

--- android/src/main/java/com/apprisemobile/foxitpdf/ReactNativeFoxitPdfActivity.java
+++ android/src/main/java/com/apprisemobile/foxitpdf/ReactNativeFoxitPdfActivity.java
@@ -1,37 +1,42 @@
 package com.apprisemobile.foxitpdf;
 
-import android.app.Activity;
-import android.graphics.Color;
+import android.content.Intent;
+import android.content.res.Configuration;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.RelativeLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.view.KeyEvent;
+import android.view.Menu;
+import android.view.WindowManager;
 
 import com.foxit.sdk.PDFViewCtrl;
 import com.foxit.uiextensions.UIExtensionsManager;
+import com.foxit.uiextensions.pdfreader.impl.MainFrame;
 import com.foxit.uiextensions.pdfreader.impl.PDFReader;
 
 import java.io.InputStream;
 import java.io.ByteArrayInputStream;
 import java.nio.charset.Charset;
 
-public class ReactNativeFoxitPdfActivity extends Activity {
+public class ReactNativeFoxitPdfActivity extends AppCompatActivity {
+    public PDFReader mPDFReader;
+
     @Override
     public void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         final Bundle bundle = getIntent().getExtras();
         final String path = bundle.getString("path");
         final boolean annotationsEnabled = bundle.getBoolean("annotationsEnabled");
-        final RelativeLayout relativeLayout = new RelativeLayout(this);
-        final RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(
-                RelativeLayout.LayoutParams.MATCH_PARENT, RelativeLayout.LayoutParams.MATCH_PARENT);
+        // final RelativeLayout relativeLayout = new RelativeLayout(this);
+        // final RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(
+        //         RelativeLayout.LayoutParams.MATCH_PARENT, RelativeLayout.LayoutParams.MATCH_PARENT);
+        //
+        // relativeLayout.addView(pdfViewCtrl, params);
+        // relativeLayout.setWillNotDraw(false);
+        // relativeLayout.setBackgroundColor(Color.argb(255, 225, 225, 225));
+        // relativeLayout.setDrawingCacheEnabled(true);
+        // setContentView(relativeLayout);
 
-        final PDFViewCtrl pdfViewCtrl = new PDFViewCtrl(this);
-
-        relativeLayout.addView(pdfViewCtrl, params);
-        relativeLayout.setWillNotDraw(false);
-        relativeLayout.setBackgroundColor(Color.argb(255, 225, 225, 225));
-        relativeLayout.setDrawingCacheEnabled(true);
-        setContentView(relativeLayout);
+        getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
 
         final String annotationsEnabledString = annotationsEnabled ? "true" : "false";
 
@@ -55,15 +60,91 @@ public class ReactNativeFoxitPdfActivity extends Activity {
         final InputStream stream = new ByteArrayInputStream(UIExtensionsConfig.getBytes(Charset.forName("UTF-8")));
         UIExtensionsManager.Config config = new UIExtensionsManager.Config(stream);
 
-        final UIExtensionsManager uiextensionsManager = new UIExtensionsManager(this, relativeLayout, pdfViewCtrl,config);
-        uiextensionsManager.setAttachedActivity(this);
+        final PDFViewCtrl pdfViewCtrl = new PDFViewCtrl(this.getApplicationContext());
+        final UIExtensionsManager uiextensionsManager = new UIExtensionsManager(this.getApplicationContext(), null, pdfViewCtrl,config);
 
         pdfViewCtrl.setUIExtensionsManager(uiextensionsManager);
+        uiextensionsManager.setFormHighlightColor(0xffff0000);
 
-        final PDFReader mPDFReader= (PDFReader) uiextensionsManager.getPDFReader();
-        mPDFReader.onCreate(this, pdfViewCtrl, null);
+        uiextensionsManager.setAttachedActivity(this);
+
+        mPDFReader= (PDFReader) uiextensionsManager.getPDFReader();
+        mPDFReader.onCreate(this, pdfViewCtrl, savedInstanceState);
         mPDFReader.openDocument(path, null);
         setContentView(mPDFReader.getContentView());
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if(mPDFReader == null)
+            return;
         mPDFReader.onStart(this);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if(mPDFReader == null)
+            return;
+        mPDFReader.onPause(this);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if(mPDFReader == null)
+            return;
+        mPDFReader.onResume(this);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if(mPDFReader == null)
+            return;
+        mPDFReader.onStop(this);
+    }
+
+    @Override
+    protected void onDestroy() {
+        this.getWindow().clearFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        if(mPDFReader != null)
+            mPDFReader.onDestroy(this);
+        super.onDestroy();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(mPDFReader != null)
+            mPDFReader.onActivityResult(this, requestCode, resultCode, data);
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        if(mPDFReader == null)
+            return;
+        ((MainFrame) mPDFReader.getMainFrame()).updateSettingBar();
+        mPDFReader.onConfigurationChanged(this, newConfig);
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (mPDFReader != null && mPDFReader.onKeyDown(this, keyCode, event))
+            return true;
+        return super.onKeyDown(keyCode, event);
+    }
+
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        if (mPDFReader != null && !mPDFReader.onPrepareOptionsMenu(this, menu))
+            return false;
+        return super.onPrepareOptionsMenu(menu);
+    }
+
+    private void finishActivity() {
+        this.finish();
     }
 }
